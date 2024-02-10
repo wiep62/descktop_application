f383714f5646dfb2219f27eb678c651d499334dc
MVN home <wiep1994@yandex.ru>
2024-02-10 14:46:41 +0300

    GU_interface_complite

---
+++ descktop/src/main/java/org/example/MainForm.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+public class MainForm {
+    private JPanel mainPanel;
+    private JTextArea textArea;
+    private JButton countButton;
+    private JButton clearButton;
+
+    public JPanel getMainPanel(){
+        return mainPanel;
+    }
+}
