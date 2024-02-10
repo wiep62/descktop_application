efaa79ee972097317604762907088d236f5d23ff
MVN home <wiep1994@yandex.ru>
2024-02-10 19:45:25 +0300

    drawPanel

--- GUI/src/MainForm.java
+++ GUI/src/MainForm.java
@@ -5,12 +5,59 @@ import java.beans.PropertyChangeListener;
 
 public class MainForm {
     private JPanel mainPanel;
-    private JTextArea textArea;
+   // private JTextArea textArea;  удалили, чтобы протестировать работу
     private JButton clearButton;
     private JButton countButton;
+    private JPanel drawPanel;
 
     public MainForm(){
-        clearButton.addActionListener(new Action() {
+
+        countButton.addActionListener(new Action() {
+            @Override
+            public Object getValue(String key) {
+                return null;
+            }
+
+            @Override
+            public void putValue(String key, Object value) {
+
+            }
+
+            @Override
+            public void setEnabled(boolean b) {
+
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+
+            @Override
+            public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void removePropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Graphics2D graphics2D = (Graphics2D) drawPanel.getGraphics(); //преобразуем drawPanel.getGraphics() к ГРАФИКС 2Д
+                //рисуем в graphics2D:
+                graphics2D.setColor(Color.RED);
+                graphics2D.drawRect(55,55,12, 22);
+
+            }
+        });
+
+        }
+
+  /*
+    public MainForm() {
+       clearButton.addActionListener(new Action() {
             @Override
             public Object getValue(String key) {
                 return null;
@@ -141,9 +188,12 @@ if (e.getKeyChar() == '6'){
             }
         });
 
-    }
+    */
+
+
 
     public JPanel getMainPanel(){
         return mainPanel;
     }
+
 }
